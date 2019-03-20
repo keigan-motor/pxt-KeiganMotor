005172553c839e5d8622ad16d1385478b0bbe913
Keigan Inc <31641835+keigan-motor@users.noreply.github.com>
2019-03-20 15:41:33 +0900

    Updates.

--- main.ts
+++ main.ts
@@ -17,7 +17,7 @@ namespace keiganmotor {
     /*
      * Set RADIO groupId 
      */
-    function setGroup(id: number) {
+    export function setGroup(id: number) {
         if (0 <= id && id <= 255) radio.setGroup(id);
     }
 
@@ -249,7 +249,7 @@ namespace keiganmotor {
     //% weight=90 blockGap=8
     //% parts="KeiganMotor"
     //% trackArgs=0,2
-    //% blockSetVariable=motormodule
+    //% blockSetVariable=m
     export function create(name: string): KeiganMotor {
         let m = new KeiganMotor(name);
         return m;
